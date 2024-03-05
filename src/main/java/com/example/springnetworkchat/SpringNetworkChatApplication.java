5f62607d076d9af61751cb5ef25661b1a5d5fa8a
Yaroslav <lynnyk265@gmail.com>
2024-03-05 11:34:48 +0200

    Added hot reloading

--- src/main/java/com/example/springnetworkchat/SpringNetworkChatApplication.java
+++ src/main/java/com/example/springnetworkchat/SpringNetworkChatApplication.java
@@ -2,7 +2,6 @@ package com.example.springnetworkchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
@@ -10,4 +9,6 @@ public class SpringNetworkChatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringNetworkChatApplication.class, args);
 	}
+
 }
+
